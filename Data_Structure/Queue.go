f7a33049e1d4c33f498e81d1bbb77286e441fbb2
tfhhh <2780181705@qq.com>
2020-09-02 15:35:20 +0800

    重写了方法
    重写了方法

--- Data_Structure/Queue.go
+++ Data_Structure/Queue.go
@@ -9,48 +9,61 @@ type Queue struct {
 	Data[] int
 }
 //初始化
-func (Q *Queue) Init() Queue {
+func InitQueue(Q *Queue) *Queue {
 	Q.rear = 0
 	Q.front = 0
-	Q.Data = make([]int, QueueMax)
-	return Queue{Q.rear,Q.front,Q.Data}
+	Q.Data = make([]int,QueueMax)
+	return &Queue{Q.rear,Q.front,Q.Data}
 }
 //队长
-func(Q *Queue) Length() int {
+func Leng(Q *Queue) int {
 	return (Q.rear - Q.front + QueueMax)%QueueMax
 }
 //入队
-func(Q *Queue) EnQueue(data int) bool {
+func EnQueue(Q *Queue,data int) *Queue {
 	//队满
 	if (Q.rear+1)%QueueMax == Q.front {
-		return false
+		fmt.Println("data:",data)
+		fmt.Println("队列已满，返回原队列！")
+		return Q
 	} else {
 		Q.Data[Q.rear] = data
 		Q.rear = (Q.rear+1)%QueueMax
-		return true
+		return Q
 	}
 }
 //出队
-func(Q *Queue) OutQueue() bool {
+func OutQueue(Q *Queue) *Queue {
 	//队空
 	if Q.rear == Q.front {
-		return false
+		return Q
 	}else {
-		Q.Data[Q.front] = 0
-		Q.front = (Q.front+1)% QueueMax
-		return true
+		Q.Data[Q.front] = -1
+		Q.front = (Q.front+1)%QueueMax
+		return Q
 	}
 }
 
 func main() {
-	var myQueue Queue
-	myQueue.Init()
-	//入队，[1,2,3,4,0,0,0,0,0]
-	for i := 1; i < 5; i++ {
-		myQueue.EnQueue(i)
+	var myQueue *Queue
+	myQueue = new(Queue)
+	InitQueue(myQueue)
+	for i := 0; i < 11; i++ {
+		myQueue = EnQueue(myQueue,i+1)
 	}
-	fmt.Printf("init queue:%v,length:%d\n",myQueue,myQueue.Length())
-	myQueue.OutQueue()	//[0,2,3,4,0,0,0,0,0]
-	myQueue.OutQueue()	//[0,0,3,4,0,0,0,0,0]
-	fmt.Printf("my queue:%v,length:%d\n",myQueue,myQueue.Length())
+	fmt.Printf("EnQueue:%v,length:%d\n",myQueue,Leng(myQueue))
+	for i := 0; i < 2; i++ {
+		myQueue = OutQueue(myQueue)
+	}
+	fmt.Printf("OutQueue:%v,length:%d\n",myQueue,Leng(myQueue))
+	/*
+	data: 10
+	队列已满，返回原队列！
+	data: 11
+	队列已满，返回原队列！
+	EnQueue:&{9 0 [1 2 3 4 5 6 7 8 9 0]},length:9
+	OutQueue:&{9 2 [-1 -1 3 4 5 6 7 8 9 0]},length:7
+
+	Process finished with exit code 0
+	*/
 }
