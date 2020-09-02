482c7f831eefbe3846974b270ec26e7bb38abac5
tfhhh <2780181705@qq.com>
2020-09-02 10:04:43 +0800

    Update Linked_List.go
    按照leetcode重构了链表结构体

--- Data_Structure/Linked_List.go
+++ Data_Structure/Linked_List.go
@@ -4,153 +4,166 @@ import (
 	"fmt"
 )
 
-type Data interface {}
-
-//链表元素结构体
-type Node struct {
-	Elem Data	//数据域
-	Next *Node	//指针域
-}
-//链表结构体，都从头节点开始操作
-type LinkedList struct {
-	Head *Node	//头节点
+type ListNode struct {
+  	Val int
+    Next *ListNode
 }
 //判空
-func(L *LinkedList) IsEmpty() bool{
-	if L.Head == nil {
+func IsEmpty(head *ListNode) bool {
+	if head == nil {
 		return true
 	}
 	return false
 }
-//表长
-func (L *LinkedList) Length() int {
-	start := L.Head
-	count := 0
-	for start != nil {
-		start = start.Next	//遍历下一个节点
-		count++	//计算遍历的次数
+//链表长度
+func Length(head *ListNode) int {
+	if head == nil {
+		return 0
 	}
-	return count
+	count := 0
+	for head.Next != nil {
+		head = head.Next
+		count++
+	}
+	return count + 1
 }
-//头部添加元素
-func (L *LinkedList) Add(e Data) *Node {
-	node := &Node{Elem: e}	//定义头部添加数据节点
-	node.Next = L.Head	//数据节点的Next指针为原节点的头部指针
-	L.Head = node	//数据节点转换为头部节点
+//头部添加
+func Add(head *ListNode,data int) *ListNode {
+	node := &ListNode{}
+	node.Val = data
+	node.Next = head
 	return node
 }
-//尾部添加元素
-func (L *LinkedList) Append(e Data) {
-	node := &Node{Elem: e}	//定义尾部添加数据指针
-	if L.IsEmpty() == true {
-		L.Head = node
-	}else {
-		cur := L.Head
-		for cur.Next != nil {
-			cur = cur.Next	//遍历指针域
-		}
-		cur.Next = node	//cur.Next = nil时将cur.Next连接为尾部添加的数据指针
+//尾部添加
+func Append(head *ListNode,data int) *ListNode  {
+	if head == nil {
+		head = Add(head,data)
+		return head
 	}
-}
-//插入元素,i为位置，e为元素
-func (L LinkedList) Insert(i int,e Data)  {
-	if i < 0 {
-		L.Add(e)	//头部添加
-	} else if i > L.Length() {
-		L.Append(e)	//尾部添加
-	}else {
-		pre := L.Head
-		for j:=0; j < i-1; j++ {
-			pre = pre.Next	//遍历指针域到前一个节点
-		}
-		node := &Node{Elem: e}	//创建插入数据的指针域
-		node.Next = pre.Next	//指针域连接
-		pre.Next = node			//指针域连接
-	}
-}
-//查找e值
-func (L *LinkedList) Search(e Data) bool {
-	start := L.Head
-	for start != nil {
-		if start.Elem == e {
-			return true
-		}
-		start = start.Next	//遍历节点，直到节点指针域为空
-	}
-	return false
-}
+	node := &ListNode{}
+	node.Val = data
+	node.Next =  nil
 
-//删除e值
-func (L *LinkedList) Delete(e int)  {
-	pre := L.Head
-	if pre.Elem == e {
-		L.Head = pre.Next	//删除值为头节点的值，链表的头节点为pre.Next
+	cur := head
+	for cur.Next != nil  {
+		cur = cur.Next
+	}
+	cur.Next = node
+	return head
+}
+//指定位置插入
+func Insert(head *ListNode,index int,data int) *ListNode {
+	if index <= 0 {
+		return head
+	}else if index == 1{
+		head = Add(head,data)
+		return head
+	}else if index == Length(head) {
+		head = Append(head,data)
+		return head
 	}else {
-		for pre.Next != nil {
-			if pre.Next.Elem == e {
-				pre.Next = pre.Next.Next	//删除所有节点值为e的节点
-			}else {
-				pre = pre.Next	//查找下一节点
-			}
+		cur := head
+		count := 1
+		for count < index - 1 {
+			cur = cur.Next
+			count++
 		}
+		node := &ListNode{}
+		node.Val = data
+		node.Next = cur.Next
+		cur.Next = node
+		return head
 	}
 }
-//删除链表的第 n 个节点
-func(L *LinkedList) removeNthFromEnd(n int) bool{
-	var count = 1
-	pre := L.Head
-	if n <= 0 {
-		return false
-	}else if n == 1 {
-		L.Head = pre.Next
-		return true
-	}else {
-		for pre.Next != nil {
-			if count == n-1 {
-				pre.Next = pre.Next.Next
-				break
-			}else {
-				pre = pre.Next
-				count++
-			}
-		}
-		return true
+//查找
+func Search(head *ListNode,data int) (bool,int) {
+	if head == nil {
+		return false,-1
 	}
+	cur := head
+	count := 1
+	result := false
+	for cur.Next != nil {
+		if cur.Val != data {
+			cur = cur.Next
+			count++
+		}else {
+			result =  true
+			break
+		}
+	}
+	if result == false {
+		return false,-1
+	}else {
+		return true,count
+	}
+
+}
+//删除指定位置元素
+func Delete(head *ListNode,index int) *ListNode {
+	if index == 1 {
+		head = head.Next
+		return head
+	}
+	cur := head
+	count := 1
+	for cur.Next != nil {
+		if count < index - 1 {
+			cur = cur.Next
+		}
+		count++
+		if count == index-1 {
+			break
+		}
+	}
+	cur.Next = cur.Next.Next
+	return head
 }
 //遍历
-func (L *LinkedList) ShowList(){
-	if L.IsEmpty() != true {
-		start := L.Head
-		for {
-			fmt.Printf("%v ",start.Elem)
-			if start.Next != nil {
-				start = start.Next //遍历下一节点
-			}else {
-				break
-			}
-		}
-		fmt.Println()
+func ShowList(head *ListNode)  {
+	if head == nil {
+		return
 	}
+	for  {
+		fmt.Printf("%d ",head.Val)
+		if head.Next != nil {
+			head = head.Next
+		}else {
+			break
+		}
+	}
+	fmt.Println()
 }
 func main() {
-	var MyLinkedList LinkedList
-	MyLinkedList.Add(0)
-	MyLinkedList.Append(1)
-	MyLinkedList.Add(3)
-	MyLinkedList.Insert(1,4)
-	MyLinkedList.Append(1)
-	MyLinkedList.ShowList()	//3 4 0 1 1
-	fmt.Println("IsEmpty:",MyLinkedList.IsEmpty())
-	fmt.Println("original length:",MyLinkedList.Length())
-
-	mySearch1 := MyLinkedList.Search(1)
-	fmt.Println("search 1:", mySearch1)
-	mySearch2 := MyLinkedList.Search(2)
-	fmt.Println("search 2:",mySearch2)
-
-	MyLinkedList.Delete(1)
-	MyLinkedList.ShowList()	//3 4 0
-	fmt.Println("deleted length:",MyLinkedList.Length())
-
+	var MyList *ListNode
+	for i := 5; i > 0; i-- {
+		MyList = Add(MyList,i)
+	}
+	for i := 6; i < 11; i++ {
+		MyList = Append(MyList,i)
+	}
+	//show
+	MyList = Insert(MyList,5,11)
+	fmt.Println("Length:",Length(MyList))
+	fmt.Println("IsEmpty:",IsEmpty(MyList))
+	ShowList(MyList)
+	//search
+	s1,ok1 := Search(MyList,11)
+	s2,ok2 := Search(MyList,12)
+	fmt.Printf("search 11:%v,index:%v\n",s1,ok1)
+	fmt.Printf("search 12:%v,index:%v\n",s2,ok2)
+	//delete
+	MyList = Delete(MyList,1)
+	ShowList(MyList)
+	MyList = Delete(MyList,4)
+	ShowList(MyList)
+	/*
+	Length: 11
+	IsEmpty: false
+	1 2 3 4 11 5 6 7 8 9 10
+	search 11:true,index:5
+	search 12:false,index:-1
+	2 3 4 11 5 6 7 8 9 10
+	2 3 4 5 6 7 8 9 10
+	*/
 }
-
