14c697620e5fe35fbfa0fddb40bdf9fcee1189f4
tfhhh <2780181705@qq.com>
2020-09-02 14:56:33 +0800

    按照leetcode重写了方法
    按照leetcode重写了方法

--- Data_Structure/Stack.go
+++ Data_Structure/Stack.go
@@ -1,87 +1,84 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-const StackSize int = 10
-//栈，操作受限的线性表
-type stack struct {
-	top int		//栈顶
-	length int	//栈长
-	Data[] int  //栈元素
+const SIZE = 10
+type Stack struct {
+	top int
+	data [SIZE]int
 }
 //初始化
-func(S *stack) Init() bool{
-	if S.top < 0 {
-		return false
-	}else {
-		S.top = 0                       //栈顶置0
-		S.length = StackSize            //设置栈长
-		S.Data = make([]int, StackSize) //创建栈元素数组
-		return true
-	}
+func Init(S *Stack) *Stack {
+	S.top = 0
+	S.data = [SIZE]int{}
+	return S
 }
 //判空
-func(S *stack) IsEmpty() bool {
+func Empty(S *Stack) bool {
 	return S.top == 0
 }
-//求栈长，S.top-S.base
-func(S *stack) Length() int {
+//栈长
+func Len(S *Stack) int {
 	return S.top
 }
 //进栈
-func(S *stack) Push(data int) bool {
-	//栈满
-	if S.top == S.length {
-		return false
+func Push(S *Stack,data int) *Stack {
+	if S.top == SIZE {
+		fmt.Println("栈满，返回原栈！")
+		return S
 	}else {
-		S.Data[S.top] = data	//压入数据
-		S.top++					//栈顶+1
-		return true
+		S.data[S.top] = data	//压入数据
+		S.top++	//栈顶+1
+		return S
 	}
 }
 //出栈
-func(S *stack) Pop() bool {
-	//栈空
+func Pop(S *Stack) *Stack  {
 	if S.top == 0 {
-		return false
+		return S
 	}else {
-		S.top--			  //栈顶-1
-		S.Data[S.top] = 0 //出栈元素置空
-		return true
+		S.top--	//栈顶减1
+		S.data[S.top] = -1	//数据置空
+		return S
 	}
 }
-//取栈顶元素
-func(S *stack) GetTop() int{
-	if S.top == 0{
-		return 0
+//取栈顶
+func GetTop(S *Stack) int {
+	if S.top == 0 {
+		return -1
 	}else {
-		return S.Data[S.top-1]
+		return S.data[S.top-1]
 	}
+
 }
 func main() {
-	//创建栈类型
-	var myStack stack
-	//初始化
-	myStack.Init()
-	fmt.Println("original stack:",myStack)
-	for i := -1; i < 5; i++ {
-		myStack.Push(i)
-	}
-	//初始栈
-	fmt.Println("Push stack:",myStack)	//[-1,0，1，2，3，4,0,0,0,0]
-	//判空
-	fmt.Println("IsEmpty:",myStack.IsEmpty())
-	//求栈长
-	fmt.Println("Length：",myStack.Length())
+	var mystack *Stack
+	mystack = new(Stack)
+	Init(mystack)
+	fmt.Println("Empty mystack:",Empty(mystack))
 	//进栈
-	myStack.Push(-2)
-	myStack.Push(6)//	[-1,0,1,2,3,4,-2,6,0,0]
-	fmt.Printf("push stack:%v,length:%d\n",myStack,myStack.Length())
-	//出栈
-	myStack.Pop()	//[-1,0,1,2,3,4,-2,0,0,0]
-	fmt.Printf("pop stack:%v,length:%d\n",myStack,myStack.Length())
-	//取栈顶
-	fmt.Println("stack top:",myStack.GetTop())	// top=-2
+	for i := 0; i < 11; i++ {
+		mystack = Push(mystack,i+1)
+	}
+	fmt.Println("Push 1-10:",mystack)
+	fmt.Println("mystack len:",Len(mystack))
+	fmt.Println("Empty mystack:",Empty(mystack))
+	for i := 0; i < 2; i++ {
+		mystack = Pop(mystack)
+	}
+	fmt.Println("Pop 10,9:",mystack)
+	fmt.Println("mystack len:",Len(mystack))
+	fmt.Println("GetTop:",GetTop(mystack))
+	/*
+	Empty mystack: true
+	栈满，返回原栈！
+	Push 1-10: &{10 [1 2 3 4 5 6 7 8 9 10]}
+	mystack len: 10
+	Empty mystack: false
+	Pop 10,9: &{8 [1 2 3 4 5 6 7 8 -1 -1]}
+	mystack len: 8
+	GetTop: 8
+
+	Process finished with exit code 0
+	*/
 }
